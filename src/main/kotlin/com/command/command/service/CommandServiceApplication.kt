b246b9d324d9be524384194df7b1eaa1eba3eb82
kazuma <kazumazuma1124@gmail.com>
2019-12-02 19:38:41 +0900

    first commit

---
+++ src/main/kotlin/com/command/command/service/CommandServiceApplication.kt
@@ -0,0 +1,71 @@
+package com.command.command.service
+
+import com.command.command.service.cli.FileToolsCommand
+import org.springframework.boot.ExitCodeExceptionMapper
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration
+import picocli.CommandLine
+import picocli.CommandLine.IFactory
+import picocli.CommandLine.ExitCode
+import picocli.CommandLine.ParameterException
+import java.util.concurrent.Callable
+
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class, KafkaAutoConfiguration::class, WebMvcAutoConfiguration::class])
+class Application(
+		private val fileToolsCommand: FileToolsCommand,
+		private val factory: IFactory
+) : ExitCodeExceptionMapper {
+	private var exitCode: Int = 0
+	fun run(args: Array<String>) {
+		val cmd = CliCommand()
+		val commandLine = CommandLine(cmd)
+				.addSubcommand(fileToolsCommand)
+		try {
+			val parsed = commandLine.parseArgs(*args)
+			if (parsed.subcommand() == null &&
+					!parsed.isUsageHelpRequested &&
+					!parsed.isVersionHelpRequested
+			) {
+				commandLine.usage(System.out)
+				exitCode = ExitCode.USAGE
+				return
+			}
+		} catch (ignored: ParameterException) {
+			// Do nothing
+		}
+		exitCode = commandLine.setExitCodeExceptionMapper(cmd).execute(*args)
+	}
+
+		override fun getExitCode(exception: Throwable?): Int {
+			return exitCode
+
+	}
+
+}
+
+@CommandLine.Command(
+		name = "fix-aq-tool", mixinStandardHelpOptions = true,
+		versionProvider = CliCommand::class, description = ["utilities for fix-aq-service"]
+)
+
+class CliCommand : Callable<Int>, CommandLine.IExitCodeExceptionMapper, CommandLine.IVersionProvider {
+	override fun call(): Int = CommandLine.ExitCode.OK
+	override fun getVersion(): Array<String> = arrayOf("1.0.0")
+	override fun getExitCode(exception: Throwable?): Int {
+		return 1
+	}
+}
+
+fun main(args: Array<String>) {
+	SpringApplication.run(Application::class.java, *args).getBean(Application::class.java).run(args)
+}
+
+@org.springframework.context.annotation.Configuration
+class Configuration {
+}
+
+
+
---
+++ src/main/kotlin/com/command/command/service/cli/FileToolsCommand.kt
@@ -0,0 +1,71 @@
+package com.command.command.service.cli
+
+
+import com.command.command.service.CliCommand
+import org.springframework.stereotype.Component
+import picocli.CommandLine
+import picocli.CommandLine.*
+
+import java.io.File
+import java.io.IOException
+import java.nio.file.FileAlreadyExistsException
+import java.nio.file.FileSystemException
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.Arrays
+import java.util.concurrent.Callable
+
+@Component
+@CommandLine.Command(
+        name = "filetools", mixinStandardHelpOptions = true,
+        versionProvider = CliCommand::class,
+        description = ["create/delete file(s) command"])
+class FileToolsCommand : Callable<Int>, IExitCodeExceptionMapper {
+    // --create オプションと --delete オプションはいずれか一方しか指定できないようにする
+    @ArgGroup(exclusive = true, multiplicity = "1")
+    private val exclusive: Exclusive? = null
+
+    @Parameters(paramLabel = "ファイル", description = ["作成あるいは削除するファイル"])
+    private val files: Array<File>? = null
+
+    internal class Exclusive {
+
+        @CommandLine.Option(names = ["-c", "--create"], description = ["create file(s)"])
+        var isCreate: Boolean = false
+
+        @CommandLine.Option(names = ["-d", "--delete"], description = ["delete file(s)"])
+        var isDelete: Boolean = false
+    }
+
+    override fun call(): Int? {
+        listOf(*this.files!!).forEach { f ->
+            try {
+//                if (exclusive!!.isCreate) {
+//                    Files.createFile(Paths.get(f.name))
+//                    println(f.name + " is created.")
+//                } else if (exclusive.isDelete) {
+//                } else if (exclusive.isDelete) {
+                    Files.deleteIfExists(Paths.get(f.name))
+                    println(f.name + " is deleted.")
+//                }
+            } catch (e: IOException) {
+                throw RuntimeException(e)
+            }
+        }
+
+        return ExitCode.OK
+    }
+
+    override fun getExitCode(exception: Throwable): Int {
+        val cause = exception.cause
+        if (cause is FileAlreadyExistsException) {
+            // 既に存在するファイルを作成しようとしている
+            return 12
+        } else if (cause is FileSystemException) {
+            // 削除しようとしたファイルが別のプロセスでオープンされている等
+            return 13
+        }
+        return 11
+    }
+
+}
